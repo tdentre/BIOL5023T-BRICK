ca912d95af88e65aa18f7c44088cb65f6b5dfde9
brittniscott <brittniscott@acadiau.ca>
2018-02-19 14:27:30 -0400

    used dplyr command for renaming variables and was able to rearrange levels for our second graph

--- tidydatascript.R
+++ tidydatascript.R
@@ -1,6 +1,6 @@
 ### Investigating factors affecting nest selection for different bird species
-### Q1: Does substrate dictate nest seclection sites for seabirds?
-### Q2: Is there evidense for nesting selection of seabirds, based on rock diameter?
+### Q1: Is there any evidence that sea birds are choosing locations to nest based on rock size? Is there differences between species?
+### Q2: Is there any evidence that sea bird nesting location is affected by substrate type in a given area? Is there differences between species?
 
 # STEP 1: Replace "NA" with "0"
 # STEP 2: For coloumns 7-16, "0" should be replaced with "NA"
@@ -34,7 +34,8 @@ percentcoverage.df <- select(original.df, species, photo, '%rock', '%veg', '%mud
 rocksize.df <- select(original.df, species, photo, X1, X2, X3, X4, X5, X6, X7, X8, X9, X10)
 
 # STEP 6: Rename columns X1-X10, 1-10 in rocksize.df
-names(rocksize.df)[3:12] <- c('1', '2', '3', '4', '5', '6', '7', '8', '9', '10')
+rocksize.df <- rocksize.df %>%
+  rename("1" = "X1", "2" = "X2","3" = "X3","4" = "X4","5" = "X5","6" = "X6","7" = "X7","8" = "X8","9" = "X9","10" = "X10")
 
 # STEP 7: In rocksize.df, create seperate columns for 'rock.num' and 'rock.circumference'
 rocksize.df <- rocksize.df %>%
@@ -51,7 +52,8 @@ ggplot(data = rocksize.df, mapping = aes(x = species, y = rock.circumference)) +
 ### creates boxplot with width proportional to the number of observations
 
 # STEP 9: Rename columns in percentcoverage.df 
-names(percentcoverage.df)[3:6] <- c('rock', 'vegetation', 'mud.dirt', 'other')
+percentcoverage.df <- percentcoverage.df %>%
+  rename("rock" = "%rock", "vegetation" = "%veg", "mud.dirt" = "%mud/dirt", "other" = "%other")
 
 # STEP 9: In percentcoverage.df, create seperate columns for 'ground.type' and 'amount.of.cover'
 percentcoverage.df <- percentcoverage.df %>%
@@ -60,7 +62,6 @@ percentcoverage.df <- percentcoverage.df %>%
 # STEP 10: Create graph of percent coverage in relation to species
 
 percentcoverageSPECIES.df <- percentcoverage.df # Turns out theres no command for facet wraps to label each facet individually yet so a new df was made with corrected names
-  
   percentcoverageSPECIES.df[percentcoverageSPECIES.df == "arte"] <- 'Arctic Tern'
   percentcoverageSPECIES.df[percentcoverageSPECIES.df == "coei"] <- 'Common Eider'
   percentcoverageSPECIES.df[percentcoverageSPECIES.df == "ltdu"] <- 'Long-Tailed Duck'
@@ -68,6 +69,11 @@ percentcoverageSPECIES.df <- percentcoverage.df # Turns out theres no command fo
   percentcoverageSPECIES.df[percentcoverageSPECIES.df == "rand"] <- 'Random'
   percentcoverageSPECIES.df[percentcoverageSPECIES.df == "sagu"] <- 'Sabine Gull'
 
+# must reorder species in percentcoverageSPECIES.df to follow the order in the previous graph
+percentcoverageSPECIES.df <- percentcoverageSPECIES.df %>%
+  mutate(species = factor(species, levels = c('Common Eider', 'Arctic Tern', 'Sabine Gull', 'Long-Tailed Duck', 'Purple Sandpiper', 'Random'))) %>%
+  arrange(species)
+  
 ggplot(data = percentcoverageSPECIES.df, mapping = aes(x = ground.type, y = percent.cover)) +
   geom_boxplot() +
   facet_wrap(~ species, nrow = 2) +
@@ -77,5 +83,3 @@ ggplot(data = percentcoverageSPECIES.df, mapping = aes(x = ground.type, y = perc
   ylab("Percent Cover") +
   scale_x_discrete(labels = c('Other', 'Mud or Dirt', 'Rock', 'Vegetation'), 
                    limits = c('other', 'mud.dirt','rock', 'vegetation'))
-
-### Still have to figure out how to put the 'Random' facet last... {fct.reorder()}
