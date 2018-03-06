8eb86c5532d2680d345aeaee13882f493cd73a04
tdentre <36000707+tdentre@users.noreply.github.com>
2018-03-06 15:45:43 -0400

    Delete Assignment 2.R

--- Assignment 2.R
+++
@@ -1,7 +0,0 @@
-#Assignment 2
-#Tyler
-
-library(tidyverse)
-
-#Develop a function (based on the above) that will generate, for a range of each of ‘magnitude of B1’, ‘sample size’ and ‘error’, a measure of the quality of the parameter estimate. (Hint, that measure of quality is the t-value – the estimate scaled by the standard error of the estimate).
-
