d49a9f13821fb9bd26bd9b919f410377cf5c5546
tdentre <tylerdentremont@acadiau,ca>
2018-03-07 14:35:25 -0400

    class notes

---
+++ class notes.R
@@ -0,0 +1,14 @@
+##### Class Notes
+##### Using different plots can lead to different interpretations so it is important to try plotting multiple different ways.
+##### Coefficient is a discription of how 1 variable relates to another.
+##### Interaction is between predictor variables; so one variable depends on the other (sex and location are variables, mass is reponse)
+##### p-value is the probability of getting a response (coefficient) just by chance
+##### t-value is relationship between coefficient and standard error
+##### Redo assignment, look at Phil's example
+##### t-value larger with sample size, as error declines t-value increases, as strength of relationship gets stronger (coefficient) t-value also gets larger
+##### extract all data and put in the dataframe to begin.
+##### "evidense for an effect"
+
+##### if you have two levels you are estimating a single parameter, if you have level A and B, you only have to know if it is A, because if it is not, you know it is B
+##### use residuals vs. fitted plat to assess model fit
+##### 
